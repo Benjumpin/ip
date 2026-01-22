f420b62580792427c9db9c721943291da24afcdf
Benjumpin <benjaminozy@hotmail.com>
2026-01-22 12:23:32 +0800

    Level-2: Add, List

--- src/main/java/BenBot.java
+++ src/main/java/BenBot.java
@@ -5,13 +5,29 @@ public class BenBot {
         System.out.println("=================================\n" +
                             "Hello! I'm BenBot!\n" +
                                 "What can i do for you?\n\n");
+
         Scanner sc = new Scanner(System.in);
+        List<String> ls = new ArrayList<>(100);
+
         while (true) {
             String input = sc.nextLine();
             if (input.trim().equalsIgnoreCase("bye")) break;
-            replyBox(input);
+            else if(input.trim().equalsIgnoreCase("list")) {
+                // print list
+                StringBuilder inputls = new StringBuilder();
+                int i = 1;
+                for (String s : ls) {
+                    inputls.append(i + ". " + ls.get(i-1) + "\n");
+                    i++;
+                }
+                reply(inputls.toString());
+            } else { // add input to list
+                ls.add(input);
+                reply("added: " + input);
+            }
 
         }
+
         System.out.println("=================================\n" +
                              "Bye. Hope to see you again soon!\n" +
                                 "╥━━━━━━━━╭━━╮━━┳\n" +
@@ -23,7 +39,7 @@ public class BenBot {
     }
 
 
-    public static void replyBox(String message) {
+    public static void reply(String message) {
         System.out.println("=================================\n");
         System.out.println(message);
         System.out.println("=================================\n");
