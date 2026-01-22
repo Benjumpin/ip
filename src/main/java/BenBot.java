a4a741e3520d454cc412c01fcdfdda368cad1d47
Benjumpin <benjaminozy@hotmail.com>
2026-01-22 11:43:39 +0800

    Level-0: Rename, Greet, Exit

---
+++ src/main/java/BenBot.java
@@ -0,0 +1,12 @@
+public class BenBot {
+    public static void main(String[] args) {
+        System.out.println("=================================\n" +
+                            "Hello! I'm BenBot!\n" +
+                                "What can i do for you?\n\n");
+
+        System.out.println("=================================\n" +
+                             "Bye. Hope to see you again soon!\n\n" +
+                                "=================================");
+
+    }
+}
