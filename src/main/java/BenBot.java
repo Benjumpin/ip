9df6f2ea216512b2a817715ddfd66941ca78ace3
Benjumpin <benjaminozy@hotmail.com>
2026-01-22 13:51:34 +0800

    Level-4: ToDos, Events, Deadlines

--- src/main/java/BenBot.java
+++ src/main/java/BenBot.java
@@ -25,18 +25,51 @@ public class BenBot {
 
             } else if (niceInputString.startsWith("mark")) {
                 // mark item
-                String[] num = niceInputString.split(" ");
-                Task itemToMark = ls.get(Integer.parseInt(num[1]) - 1);
+                String[] strings = niceInputString.split(" ");
+                Task itemToMark = ls.get(Integer.parseInt(strings[1]) - 1);
                 itemToMark.markDone();
                 reply("Nice! I've marked this task as done:\n " + itemToMark);
 
             } else if (niceInputString.startsWith("unmark")) {
-                // mark item
-                String[] num = niceInputString.split(" ");
-                Task itemToMark = ls.get(Integer.parseInt(num[1]) - 1);
+                // unmark item
+                String[] strings = niceInputString.split(" ");
+                Task itemToMark = ls.get(Integer.parseInt(strings[1]) - 1);
                 itemToMark.markUndone();
-                reply("Nice! I've unmarked this task as done:\n " + itemToMark);
 
+            } else if (niceInputString.startsWith("todo")) {
+                // todo item
+                String todoItem = inputString.substring(5).trim();
+                Task t = new Todo(todoItem);
+                ls.add(t);
+                reply("Got it. I've added this task:\n  " + t +
+                        "\nNow you have " + ls.size() + " tasks in the list.");
+
+            } else if (niceInputString.startsWith("deadline")) {
+                // deadline item
+                String[] deadlineItem = inputString.split(" /by ");
+                if (deadlineItem.length < 2) reply("not valid deadline, use /by to specify date.\n");
+                else {
+                    String desc = deadlineItem[0].substring(9).trim();
+                    String from = deadlineItem[1].trim();
+                    Task t = new Deadline(desc, from);
+                    ls.add(t);
+                    reply("Got it. I've added this task:\n  " + t +
+                            "\nNow you have " + ls.size() + " tasks in the list.");
+                }
+
+            } else if (niceInputString.startsWith("event")) {
+                // event item
+                String[] eventItem = inputString.split(" /from | /to ");
+                if (eventItem.length < 3) reply("not valid event, use /from and /to to specify date\n");
+                else {
+                    String desc = eventItem[0].substring(6).trim();
+                    String from = eventItem[1].trim();
+                    String to = eventItem[2].trim();
+                    Task t = new Event(desc, from, to);
+                    ls.add(t);
+                    reply("Got it. I've added this task:\n  " + t +
+                            "\nNow you have " + ls.size() + " tasks in the list.");
+                }
             } else {
                 // add input to list
                 Task inputTask = new Task(inputString);
