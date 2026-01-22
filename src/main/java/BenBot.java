79a0e0e9b954bd7ba89b4679515da3c7eef6efdf
Benjumpin <benjaminozy@hotmail.com>
2026-01-22 16:00:33 +0800

    Level-6: Delete

--- src/main/java/BenBot.java
+++ src/main/java/BenBot.java
@@ -7,86 +7,110 @@ public class BenBot {
                                 "What can i do for you?\n\n");
 
         Scanner sc = new Scanner(System.in);
-        List<Task> ls = new ArrayList<>(100);
+        List<Task> ls = new ArrayList<>();
 
         while (true) {
             String inputString = sc.nextLine();
             String niceInputString = inputString.trim().toLowerCase();
-            if (niceInputString.equals("bye")) break;
+            try {
+                if (niceInputString.equals("bye")) break;
 
-            else if(niceInputString.equals("list")) {
-                // print list
-                StringBuilder inputList = new StringBuilder();
+                else if (niceInputString.equals("list")) {
+                    // print list
+                    StringBuilder inputList = new StringBuilder();
 
-                for (int i = 0; i< ls.size(); i++) {
-                    inputList.append(i + 1 + ". " + ls.get(i) + "\n");
-                }
-                reply(inputList.toString());
+                    for (int i = 0; i < ls.size(); i++) {
+                        inputList.append(i + 1 + ". " + ls.get(i) + "\n");
+                    }
+                    reply(inputList.toString());
 
-            } else if (niceInputString.startsWith("mark")) {
-                // mark item
-                String[] strings = niceInputString.split(" ");
-                Task itemToMark = ls.get(Integer.parseInt(strings[1]) - 1);
-                itemToMark.markDone();
-                reply("Nice! I've marked this task as done:\n " + itemToMark);
+                } else if (niceInputString.startsWith("mark")) {
+                    // mark item
+                    String[] strings = niceInputString.split(" ");
+                    Task itemToMark = ls.get(Integer.parseInt(strings[1]) - 1);
+                    itemToMark.markDone();
+                    reply("Nice! I've marked this task as done:\n " + itemToMark);
 
-            } else if (niceInputString.startsWith("unmark")) {
-                // unmark item
-                String[] strings = niceInputString.split(" ");
-                Task itemToMark = ls.get(Integer.parseInt(strings[1]) - 1);
-                itemToMark.markUndone();
+                } else if (niceInputString.startsWith("unmark")) {
+                    // unmark item
+                    String[] strings = niceInputString.split(" ");
+                    Task itemToMark = ls.get(Integer.parseInt(strings[1]) - 1);
+                    itemToMark.markUndone();
+                    reply("OK, I've marked this task as not done yet:\n " + itemToMark);
 
-            } else if (niceInputString.startsWith("todo")) {
-                // todo item
-                String todoItem = inputString.substring(5).trim();
-                Task t = new Todo(todoItem);
-                ls.add(t);
-                reply("Got it. I've added this task:\n  " + t +
-                        "\nNow you have " + ls.size() + " tasks in the list.");
-
-            } else if (niceInputString.startsWith("deadline")) {
-                // deadline item
-                String[] deadlineItem = inputString.split(" /by ");
-                if (deadlineItem.length < 2) reply("not valid deadline, use /by to specify date.\n");
-                else {
-                    String desc = deadlineItem[0].substring(9).trim();
-                    String from = deadlineItem[1].trim();
-                    Task t = new Deadline(desc, from);
+                } else if (niceInputString.startsWith("todo")) {
+                    if (niceInputString.length() <= 5) {
+                        // STOP here and throw the error. The catch block above will handle it.
+                        throw new BenBotExceptions("Empty description");
+                    }
+                    // todo item
+                    String todoItem = inputString.substring(5).trim();
+                    Task t = new Todo(todoItem);
                     ls.add(t);
                     reply("Got it. I've added this task:\n  " + t +
                             "\nNow you have " + ls.size() + " tasks in the list.");
-                }
 
-            } else if (niceInputString.startsWith("event")) {
-                // event item
-                String[] eventItem = inputString.split(" /from | /to ");
-                if (eventItem.length < 3) reply("not valid event, use /from and /to to specify date\n");
-                else {
-                    String desc = eventItem[0].substring(6).trim();
-                    String from = eventItem[1].trim();
-                    String to = eventItem[2].trim();
-                    Task t = new Event(desc, from, to);
-                    ls.add(t);
-                    reply("Got it. I've added this task:\n  " + t +
-                            "\nNow you have " + ls.size() + " tasks in the list.");
+                } else if (niceInputString.startsWith("deadline")) {
+                    // deadline item
+                    String[] deadlineItem = inputString.split(" /by ");
+                    if (deadlineItem.length < 2) {
+                        throw new BenBotExceptions("not valid deadline, use /by to specify date.\n");
+                    }
+                    if (deadlineItem[0].length() <= 9) {
+                        throw new BenBotExceptions("Empty description");
+                    } else {
+                        String desc = deadlineItem[0].substring(9).trim();
+                        String by = deadlineItem[1].trim();
+                        Task t = new Deadline(desc, by);
+                        ls.add(t);
+                        reply("Got it. I've added this task:\n  " + t +
+                                "\nNow you have " + ls.size() + " tasks in the list.");
+                    }
+
+                } else if (niceInputString.startsWith("event")) {
+                    // event item
+                    String[] eventItem = inputString.split(" /from | /to ");
+                    if (eventItem.length < 3) {
+                        throw new BenBotExceptions("not valid event, use /from and /to to specify date\n");
+                    }
+                    if (eventItem[0].length() <= 6) {
+                        throw new BenBotExceptions("Empty description");
+                    } else {
+                        String desc = eventItem[0].substring(6).trim();
+                        String from = eventItem[1].trim();
+                        String to = eventItem[2].trim();
+                        Task t = new Event(desc, from, to);
+                        ls.add(t);
+                        reply("Got it. I've added this task:\n  " + t +
+                                "\nNow you have " + ls.size() + " tasks in the list.");
+                    }
+                } else if (niceInputString.startsWith("delete")) {
+                    String[] deleteItem = inputString.split(" ");
+                    if (deleteItem.length < 2) {
+                        throw new BenBotExceptions("Please provide number");
+                    }
+                    try {
+                        int index = Integer.parseInt(deleteItem[1]);
+
+                        if (index < 0 || index >= ls.size()) {
+                            throw new BenBotExceptions("No such item in list");
+                        }
+                        Task t = ls.remove(index); // save removed item to print latre
+                        reply("Noted. I've removed this task:\n  " + t +
+                                "\nNow you have " + ls.size() + " tasks in the list.");
+                    } catch (NumberFormatException e) {
+                        throw new BenBotExceptions("Please enter a number");
+                    }
+                } else {
+                    throw new BenBotExceptions("stop blabbering!");
                 }
-            } else {
-                // add input to list
-                Task inputTask = new Task(inputString);
-                ls.add(inputTask);
-                reply("added: " + inputString);
+            } catch (BenBotExceptions e) {
+                reply(e.getMessage());
             }
-
         }
 
         System.out.println("=================================\n" +
-                             "Bye. Hope to see you again soon!\n" +
-                                "╥━━━━━━━━╭━━╮━━┳\n" +
-                                "╢╭╮╭━━━━━┫┃▋▋━▅┣\n" +
-                                "╢┃╰┫┈┈┈┈┈┃┃┈┈╰┫┣\n" +
-                                "╢╰━┫┈┈┈┈┈╰╯╰┳━╯┣\n" +
-                                "╢┊┊┃┏┳┳━━┓┏┳┫┊┊┣\n" +
-                                "╨━━┗┛┗┛━━┗┛┗┛━━┻\n");
+                             "Bye. Hope to see you again soon!");
     }
 
 
--- src/main/java/BenBotExceptions.java
+++ src/main/java/BenBotExceptions.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
+public class BenBotExceptions extends Exception {
+    public BenBotExceptions(String msg) {
+        super(msg);
+    }
 
-public class BenBotExceptions {
 }
